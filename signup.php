507f05c90341e6b329015b721d500067a7134a3a
aliahrusdi <aliahrsdi@gmail.com>
2024-03-11 07:54:57 +0800

    change h1 modal

--- signup.php
+++ signup.php
@@ -105,7 +105,7 @@ if (
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Password and Confirm Password Error</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel"> Invalid Password</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
